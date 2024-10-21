262fc645ec71260951ce35b5258b2dabdd385773
mainaliashish <mainaliashish@outlook.com>
2024-10-21 17:20:10 +0100

    redux setup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from 'react';
 import Header from "./Header";
 import { checkValidData } from "../utils/validate"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../utils/firebase";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -13,9 +15,37 @@ const Login = () => {
   }
   const handleSubmit = () => {
       // console.log(email, password);
-      const message = checkValidData(email.current.value, password.current.value, fullName.current.value);
+      const message = checkValidData(email.current.value, password.current.value);
       setErrorMessage(message);
-      console.log(message);
+      if (message) return;
+      if(!isSignInForm) {
+        createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          // Signed up 
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+          setErrorMessage(errorCode + '-' + errorMessage);
+        });
+      
+      } else {
+        signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + '-' + errorMessage);
+        });
+      
+      }
   }
   return (
     <div>
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -3,12 +3,12 @@ export const checkValidData = (email, password) => {
     const isEmailValid = emailPattern.test(email);
     const passwordPattern = /^[A-Za-z0-9]{6,}$/;
     const isPasswordValid = passwordPattern.test(password)
-    const fullNamePattern = /^[a-zA-Z]+ [a-zA-Z]+$/;
-    const isFullNameValid = passwordPattern.test(fullNamePattern)
+    // const fullNamePattern = /^[a-zA-Z]+ [a-zA-Z]+$/;
+    // const isFullNameValid = passwordPattern.test(fullNamePattern)
 
     if (!isEmailValid) return "Email ID is not valid";
     if (!isPasswordValid) return "Password is not valid";
-    if (!isFullNameValid) return "Full Name is not valid";
+    // if (!isFullNameValid) return "Full Name is not valid";
 
     return null;
 };
